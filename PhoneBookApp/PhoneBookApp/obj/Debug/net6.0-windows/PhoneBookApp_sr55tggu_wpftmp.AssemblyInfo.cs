dbad232189d6070a51e262198c631dcc44c8258f
Mantas <m.kulikauskas01@gmail.com>
2023-12-08 08:32:56 +0200

    Add 0.9 build

--- PhoneBookApp/PhoneBookApp/obj/Debug/net6.0-windows/PhoneBookApp_sr55tggu_wpftmp.AssemblyInfo.cs
+++ PhoneBookApp/PhoneBookApp/obj/Debug/net6.0-windows/PhoneBookApp_sr55tggu_wpftmp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhoneBookApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("0.9")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhoneBookApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhoneBookApp")]
--- PhoneBookApp/PhoneBookApp/obj/Release/net6.0-windows/PhoneBookApp.AssemblyInfo.cs
+++ PhoneBookApp/PhoneBookApp/obj/Release/net6.0-windows/PhoneBookApp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhoneBookApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.9")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhoneBookApp")]
