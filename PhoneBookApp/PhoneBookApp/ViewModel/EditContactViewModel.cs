747cf020d716461d666e3d34beda800158bd9018
Mantas <m.kulikauskas01@gmail.com>
2023-12-12 10:02:18 +0200

    Fixed #7 and #8 can't edit contact with empty fields or incorrect phone no. format

--- PhoneBookApp/PhoneBookApp/ViewModel/EditContactViewModel.cs
+++ PhoneBookApp/PhoneBookApp/ViewModel/EditContactViewModel.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
@@ -18,8 +19,8 @@ namespace PhoneBookApp.ViewModel
         public RelayCommand editContactCommand { get; private set; }
 
         private int ID;
-
         private ContactManager _contactManager;
+        private String PhoneNoRegex = "^[+]?[(]?[0-9]{1,4}[)]?[-\\s\\./0-9]*$";
 
         public EditContactViewModel(Contact contactInfo)
         {
@@ -33,7 +34,16 @@ namespace PhoneBookApp.ViewModel
 
         private void EditContact(object placeHolder)
         {
-            _contactManager.EditContact(new Contact() { ID = ID, Name = Name, PhoneNo = PhoneNo, BirthDate = BirthDate });
+            if (Name == null || PhoneNo == null || Name == "" || PhoneNo == "")
+            {
+                MessageBox.Show("Name and/or Phone No. fields must be filled in", "Empty fields detected");
+            }
+            else if (!Regex.IsMatch(PhoneNo, PhoneNoRegex))
+            {
+                MessageBox.Show("Please enter a valid Phone No.", "Invalid Phone No.");
+            }
+            else
+                _contactManager.EditContact(new Contact() { ID = ID, Name = Name, PhoneNo = PhoneNo, BirthDate = BirthDate });
         }
     }
 }
